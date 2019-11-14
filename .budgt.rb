6769f383de7e7d3d7e4cd8cb4392766397721fb8
dallas <dallas-hill@hotmail.com>
2019-11-14 12:59:11 +1000

    beginning

---
+++ .budgt.rb
@@ -0,0 +1,16 @@
+class Budget
+    def initialize()
+    @weekly_income = 0
+    @weekly_rent = 0
+    @weekly_groceries = 0
+    @weekly_insurance = 0
+    @weekly_recreation = 0
+    @weekly_travel = 0
+    @weekly_fitness = 0
+    @weekly_children = 0
+    end
+    puts "Welcome to .budgt! Where all of your budgeting dreams come true!
+    By typing 'quit', you can quit or restart the app at anytime!
+    Please press enter to continue."
+    user_input = gets.chomp
+end
