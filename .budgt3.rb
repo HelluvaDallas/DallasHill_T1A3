993214202a0fd3f0d22b0dac7cb16aff47ef071f
dallas <dallas-hill@hotmail.com>
2019-11-16 12:24:13 +1000

    added colorize gem and colorized certain blocks of text

--- .budgt3.rb
+++ .budgt3.rb
@@ -1,79 +1,115 @@
-class Values
-attr_accessor :income, :rent, :groceries, :insurance, :recreation,
-:utilities, :transport, :fitness, :children
-def initialize ()
-@income = 0
-@rent = 0
-@groceries = 0
-@insurance = 0
-@recreation = 0
-@utilities = 0
-@transport = 0
-@fitness = 0
-@children = 0
-end
+# class Budget
+#   attr_accessor :income, :rent, :groceries, :insurance, :recreation,
+#   :utilities, :transport, :fitness, :children,
+#   def initialize()
+#   @income = 0
+#   @rent = 0
+#   @groceries = 0
+#   @insurance = 0
+#   @recreation = 0
+#   @utilities = 0
+#   @transport = 0
+#   @fitness = 0
+#   @children = 0
+#   end
+# end
+require "colorize"
 
 def get_user_input
-  puts "What is your weekly income after tax?"
+  puts "
+  What is your weekly income after tax?".colorize(:light_blue)
   income = gets.chomp.to_f
-  puts "What is your weekly rent or morgage repayments?"
+  puts "
+  What is your weekly rent or morgage repayments?".colorize(:light_blue)
   rent = gets.chomp.to_f
-  puts "How much do you spend on groceries each week??"
+  puts "
+  How much do you spend on groceries each week?".colorize(:light_blue)
   groceries = gets.chomp.to_f
-  puts "How much do you spend on insurance??"
+  puts "
+  How much do you spend on insurance?".colorize(:light_blue)
   insurance = gets.chomp.to_f
-  puts "How much do you spend on recreation??"
+  puts "
+  How much do you spend on recreation?".colorize(:light_blue)
   recreation = gets.chomp.to_f
-  puts "How much do you spend on utilities each week??"
+  puts "
+  How much do you spend on utilities each week?".colorize(:light_blue)
   utilities = gets.chomp.to_f
-  puts "How much do you spend on transport each week??"
+  puts "
+  How much do you spend on transport each week?".colorize(:light_blue)
   transport = gets.chomp.to_f
-  puts "How much do you spend on fitness each week??"
+  puts "
+  How much do you spend on fitness each week?".colorize(:light_blue)
   fitness = gets.chomp.to_f
-  puts "How much do you spend on children each week??"
+  puts "
+  How much do you spend on children each week?".colorize(:light_blue)
   children = gets.chomp.to_f
   return income, rent, groceries, insurance, recreation, utilities, transport, fitness, children
 end
-def subtract
-  income, rent, groceries, insurance, recreation, utilities, transport, fitness, children = get_user_input()
-  result = income - rent - groceries - insurance - recreation - utilities - transport - fitness - children
-  puts "You are currently saving #{((result / income) * 100).truncate(2)}% of your income,
-  which leaves $#{result} for savings!"
+
+
+def subtract(income, rent, groceries, insurance, recreation, utilities, transport, fitness, children)
   
-if ((result / income) * 100).truncate(2) <= 0
-  puts "Looks like you arent saving anything. You might even be spending more than you earn!
-  Have you heard about the 50/20/30 Rule of Budgeting?
-50% of your income is allocated for Needs,
-20% of your income is allocated for savings, and
-30% of your income is for Wants!
-Stick to this rule and you'll be considered a
-good little Aussie Saver!"
-elsif ((result / income) * 100).truncate(2) < 20
-    puts "You're currently saving less than 20% of your weekly income.
+  result = income - rent - groceries - insurance - recreation - utilities - transport - fitness - children
+  puts "
+  You are currently saving #{((result / income) * 100).truncate(2)}% of your income,
+  which leaves $#{result} for savings!".colorize(:light_blue)
+
+  if ((result / income) * 100).truncate(2) <= 0
+    puts "
+    Oh no. Looks like you arent saving anything...
+    You might even be spending more than you earn!!
+
     Have you heard about the 50/20/30 Rule of Budgeting?
-  50% of your income is allocated for Needs,
-  20% of your income is allocated for savings, and
-  30% of your income is for Wants!
-  Stick to this rule and you'll be considered a
-  good little Aussie Saver!"
+    50% of your income is allocated for Needs,
+    20% of your income is allocated for savings, and
+    30% of your income is for Wants!
+
+    You need to re-evaluate where your moeny is going and thats why we're here!
+    Let's have another go and see if we improve some things".colorize(:light_red)
+  elsif ((result / income) * 100).truncate(2) < 20
+    puts "
+    Saving less than 20% of your weekly income is not ideal,
+    but at least you're putting something away!
+
+    Have you heard about the 50/20/30 Rule of Budgeting?
+    50% of your income is allocated for Needs,
+    20% of your income is allocated for savings, and
+    30% of your income is for Wants!
+
+    Maybe re-evaluate your Wants and try again".colorize(:light_yellow)
   elsif ((result / income) * 100).truncate(2) == 20
-    puts "you are saving 20% of your weekly income, seems like you're right on track!"
+    puts "
+    20% of your weekly income is perfect!
+    Its the recommmended amount of income to put away each week!
+    
+    Let's go again to see if we can save even more money!".colorize(:green)
   else
-    puts "Looks like you're saving more than 20% of your weekly income! You're crushing it, keep up the good work!"
+    puts "
+    Saving more than 20% of your weekly income is great!
+    You're exceeding the recommended amount. Keep up the good work!
+    
+    You're already crushing it, but let's have another go and see if we can save even more!".colorize(:green)
   end
 end
+
+
+
 quit = false
 until quit do
-  puts "Welcome to .budgt, where none of your dreams come true!
+  puts "
+  Welcome to Budgt, where the missing 'e' isn't a spelling error!
   We're here to help you see where all your money is going every week!
-  If you want to keep going down this rabbit hole of debt and sorrow 
-  type 'yes', if not, type 'q'!"
+
+  If you want to keep going down this rabbit hole of debt and sorrow
+  type 'y' followed by enter, if not, type 'q' followed by enter!
+  ".colorize(:green)
   user_input = gets.chomp
-  case user_input
-  when "yes"
-    subtract()
-    result = income - rent - groceries - insurance - recreation - utilities - transport - fitness - children
-  when "q"
-    quit = true
-  end
+    case user_input
+    when "y"
+      income, rent, groceries, insurance, recreation, utilities, transport, fitness, children = get_user_input()
+      subtract(income, rent, groceries, insurance, recreation, utilities, transport, fitness, children)
+      result = income - rent - groceries - insurance - recreation - utilities - transport - fitness - children
+    when "q"
+      quit = true
+    end
 end
